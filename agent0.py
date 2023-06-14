db9b4675892200810f48d03e41c5087eb024f9dd
Tim Haintz <tihaintz@microsoft.com>
2023-06-15 09:56:00 +1000

    Updated code structure and variables. Created a data output.

--- agent0.py
+++ agent0.py
@@ -5,7 +5,8 @@ NOTES
 Version:        0.1
 Author:         Tim Haintz                         
 Creation Date:  6/6/2023
-LINK
+LINKS
+https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/create-resource?pivots=web-portal#deploy-a-model
 https://learn.microsoft.com/en-us/azure/cognitive-services/openai/chatgpt-quickstart?tabs=command-line&pivots=programming-language-python
 https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/chatgpt?pivots=programming-language-chat-completions
 https://learn.microsoft.com/en-us/azure/cognitive-services/cognitive-services-virtual-networks?tabs=portal
@@ -14,23 +15,71 @@ https://learn.microsoft.com/en-us/azure/cognitive-services/cognitive-services-vi
 from dotenv import load_dotenv
 import os
 import openai
+import json
+from datetime import datetime
 # Load environment variables from the .env file
 load_dotenv()
 
+#############
+# VARIABLES #
+#############
 openai.api_type = "azure"
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
 openai.api_version = "2023-05-15"
 openai.api_key = os.getenv("AZURE_OPENAI_KEY")
+cve = "CVE-2021-28456"
+iso_datetime = datetime.utcnow().isoformat()
 
-response = openai.ChatCompletion.create(
-    engine="gpt-35-00", # engine = "deployment_name".
-    messages=[
+########### 
+# PROMPTS #
+###########
+prompt00 = [
+        {"role": "system", "content": "You are a helpful AI assistant helping cybersecurity responders resolve Common Vulnerability Exposure (CVE) queries"},
+        {"role": "user", "content": "Tell me about {}?".format(cve)}
+]
+
+prompt01 = [
+        {"role": "system", "content": "You are a helpful AI assistant helping cybersecurity responders resolve Common Vulnerability Exposure (CVE) queries"},
+        {"role": "user", "content": "How do I fix {}?".format(cve)}
+]
+
+prompt02 = [
+        {"role": "system", "content": "You are a helpful AI assistant helping cybersecurity responders resolve Common Vulnerability Exposure (CVE) queries"},
+        {"role": "user", "content": "What is {}?".format(cve)}
+]
+
+prompt03 = [
+        {"role": "system", "content": "You are a helpful AI assistant helping cybersecurity responders resolve Common Vulnerability Exposure (CVE) queries"},
+        {"role": "user", "content": "What steps do I need to fix {}?".format(cve)}
+]
+
+promptTest = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
         {"role": "assistant", "content": "Yes, customer managed keys are supported by Azure OpenAI."},
         {"role": "user", "content": "Do other Azure Cognitive Services support this too?"}
-    ]
+]
+
+#############
+# MAIN CODE #
+#############
+prompt = prompt00
+response = openai.ChatCompletion.create(
+    engine="gpt-35-00", # engine = "deployment_name".
+    messages=prompt
 )
 
-print(response)
-print(response['choices'][0]['message']['content'])
+# Create a dictionary with the prompt, datetime, and response
+data = {
+    "cve": cve,
+    "datetime": iso_datetime,
+    "prompt": prompt,
+    "response": response['choices'][0]['message']['content']
+}
+
+##########
+# OUTPUT #
+##########
+#print(response)
+#print(response['choices'][0]['message']['content'])
+print(data)
