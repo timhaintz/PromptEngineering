71bf70910fca296799f3e630a200bde962db866c
Tim Haintz <tihaintz@microsoft.com>
2023-06-26 09:59:33 +1000

    Changed CVE to CVE-2020-16969.

--- agent0.py
+++ agent0.py
@@ -30,7 +30,7 @@ openai.api_type = "azure"
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
 openai.api_version = "2023-05-15"
 openai.api_key = os.getenv("AZURE_OPENAI_KEY")
-cve = "CVE-2021-28456"
+cve = "CVE-2020-16969"
 iso_datetime = datetime.utcnow().isoformat()
 
 ########### 
