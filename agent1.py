c0364cd83504207d26a021321fcee1518ae103b8
Tim Haintz <tihaintz@microsoft.com>
2023-07-12 15:05:37 +1000

    Initial commit.

---
+++ agent1.py
@@ -0,0 +1,16 @@
+'''
+DESCRIPTION
+Retrive the output of agent0 and determine how effective the output was in answering how to fix the CVE.
+Connect to the Azure OpenAI API gpt-35-turbo model and chat with it.
+NOTES
+Version:        0.1
+Author:         Tim Haintz                         
+Creation Date:  12/7/2023
+LINKS
+https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/create-resource?pivots=web-portal#deploy-a-model
+https://learn.microsoft.com/en-us/azure/cognitive-services/openai/chatgpt-quickstart?tabs=command-line&pivots=programming-language-python
+https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/chatgpt?pivots=programming-language-chat-completions
+https://learn.microsoft.com/en-us/azure/cognitive-services/cognitive-services-virtual-networks?tabs=portal
+https://cve.mitre.org/
+https://nvd.nist.gov/vuln/detail/
+'''
