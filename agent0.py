e071b599becab82e759209dba6e12fc73a8ce46f
Tim Haintz <tihaintz@microsoft.com>
2023-07-12 16:16:41 +1000

    Updated file path for output.

--- agent0.py
+++ agent0.py
@@ -133,7 +133,7 @@ data = {
 #print(response['choices'][0]['message']['content'])
 #print(json.dumps(data))
 # Define the output file path
-output_file = fr"C:\Users\tihaintz\OneDrive - Microsoft\Masters\{iso_datetime.replace(':', '-').replace('.', '-')}_{cve}_Agent0.json"
+output_file = fr"C:\Users\tihaintz\OneDrive - Microsoft\Masters\Agent0Responses\{iso_datetime.replace(':', '-').replace('.', '-')}_{cve}_Agent0.json"
 # Write the data to the output file
 with open(output_file, "w") as f:
     json.dump(data, f)
