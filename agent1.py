d56d8c11c02a1486fdc8bd24b4ec630e6d7fa5e2
Tim Haintz <tihaintz@microsoft.com>
2023-07-13 17:03:43 +1000

    Updated file to check Agent0s output and look for the cve key.

--- agent1.py
+++ agent1.py
@@ -34,34 +34,47 @@ openai.api_version = "2023-05-15"
 openai.api_key = os.getenv("AZURE_OPENAI_KEY")
 iso_datetime = datetime.utcnow().isoformat()
 
+##################
+# Check the CVEs #
 ####################
-# Get a random CVE #
-####################
+# Set the folder path to look for Agent0's responses
+response_folder_path = r"C:\Users\tihaintz\OneDrive - Microsoft\Masters\Agent0Responses"
+# Loop through each file in the folder
+for filename in os.listdir(response_folder_path):
+    # Check if the file is a JSON file
+    if filename.endswith(".json"):
+        # Set the path to the JSON file
+        file_path = os.path.join(response_folder_path, filename)
+        
+        # Load the JSON file as a Python object
+        with open(file_path, "r") as f:
+            json_object = json.load(f)
+        
+        # Check if the object contains a key with the name "CVE"
+        if "cve" in json_object:
+            # Get the value of the "cve" key
+            cve_value = json_object["cve"]
+            
+            # Print the CVE value
+            print(f"File {filename} contains CVE {cve_value}")
+        else:
+            print(f"File {filename} does not contain a CVE")
+
+# Below will be used to check on the CVE from the above code 
+
 # Set the folder path to search in
-folder_path = r"C:\Users\tihaintz\OneDrive - Microsoft\Masters\cvelistV5-main\cves"
+cve_folder_path = r"C:\Users\tihaintz\OneDrive - Microsoft\Masters\cvelistV5-main\cves"
 
 # Get a list of all directories in the folder
-directories = [dir for dir in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, dir))]
+directories = [dir for dir in os.listdir(cve_folder_path) if os.path.isdir(os.path.join(cve_folder_path, dir))]
 
 # Filter the list to include only directories prior to the year 2020
 filtered_directories = [dir for dir in directories if dir <= "2020"]
 
-# Select a random directory from the filtered list
-random_directory = random.choice(filtered_directories)
-
 # Get a list of all subdirectories in the selected directory
-subdirectories_path = os.path.join(folder_path, random_directory)
+subdirectories_path = os.path.join(cve_folder_path)
 subdirectories = [dir for dir in os.listdir(subdirectories_path) if os.path.isdir(os.path.join(subdirectories_path, dir))]
 
-# Select a random subdirectory from the list of subdirectories in the selected directory
-random_subdirectory = random.choice(subdirectories)
-
 # Get a list of all files in the selected subdirectory
-subdirectory_path = os.path.join(subdirectories_path, random_subdirectory)
+subdirectory_path = os.path.join(subdirectories_path)
 files = os.listdir(subdirectory_path)
-
-# Select a random file from the list of files in the selected subdirectory
-random_file = random.choice(files)
-
-# Print the selected file name
-print(f"Selected file: {os.path.splitext(random_file)[0]}")
