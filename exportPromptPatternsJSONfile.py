634a78c03c470cdfdde608db326c6bc5eb4f2102
Tim Haintz <tihaintz@microsoft.com>
2023-07-29 17:03:06 +1000

    Updated code to allow arguments in the file. Arguments are to choose -Console as default or -HTML. HTML will output a file.

--- exportPromptPatternsJSONfile.py
+++ exportPromptPatternsJSONfile.py
@@ -8,21 +8,80 @@ Creation Date:  26/7/2023
 LINKS
 
 '''
-
+# Import modules
+import argparse
 import json
+import datetime
 
-# Open the JSON file
-with open('promptpatterns.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
+# Get the current date and time
+now = datetime.datetime.now()
+# Format the date as YYYYmmdd
+date_str = now.strftime('%Y%m%d')
 
-# Print the prompt patterns
-for title in data['Source']['Titles']:
-    print('Title:', title['Title'])
-    for category in title['CategoriesAndPatterns']:
-        print('\tPattern Category:', category['PatternCategory'])
-        for pattern in category['PromptPatterns']:
-            print('\t\tPattern Name:', pattern['PatternName'])
-            print('\t\tExample Prompts:')
-            for example in pattern['ExamplePrompts']:
-                print('\t\t\t', example)
-            print()
+def read_prompt_patterns():
+    # Open the JSON file
+    with open('promptpatterns.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    # Return the prompt patterns
+    return data
+
+def write_prompt_patterns(data, format, filename):
+    if format == 'console':
+        # Print the prompt patterns to the console
+        for title in data['Source']['Titles']:
+            print('Title:', title['Title'])
+            for category in title['CategoriesAndPatterns']:
+                print('\tPattern Category:', category['PatternCategory'])
+                for pattern in category['PromptPatterns']:
+                    print('\t\tPattern Name:', pattern['PatternName'])
+                    print('\t\tExample Prompts:')
+                    for example in pattern['ExamplePrompts']:
+                        print('\t\t\t', example)
+                    print()
+    elif format == 'html':
+        if not filename.endswith('.html'):
+            filename += '.html'
+        if '{date}' in filename:
+            # Get the current date and time
+            now = datetime.datetime.now()
+
+            # Format the date as YYYYmmdd
+            date_str = now.strftime('%Y%m%d')
+
+            # Replace {date} with the current date in the filename
+            filename = filename.replace('{date}', date_str)
+
+        # Write the prompt patterns to an HTML file
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write('<html>\n')
+            f.write('<head>\n')
+            f.write('<title>Prompt Patterns</title>\n')
+            f.write('</head>\n')
+            f.write('<body>\n')
+            for title in data['Source']['Titles']:
+                f.write('<h1>{}</h1>\n'.format(title['Title']))
+                for category in title['CategoriesAndPatterns']:
+                    f.write('<h2>{}</h2>\n'.format(category['PatternCategory']))
+                    for pattern in category['PromptPatterns']:
+                        f.write('<h3>{}</h3>\n'.format(pattern['PatternName']))
+                        f.write('<ul>\n')
+                        for example in pattern['ExamplePrompts']:
+                            f.write('<li>{}</li>\n'.format(example))
+                        f.write('</ul>\n')
+            f.write('</body>\n')
+            f.write('</html>\n')
+    else:
+        print('Invalid format:', format)
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Export prompt patterns from JSON file.')
+parser.add_argument('--format', choices=['console', 'html'], default='console', help='output format (default: console)')
+parser.add_argument('--filename', default='promptpatterns.html', help='output filename (default: promptpatterns.html)')
+args = parser.parse_args()
+
+# Read the prompt patterns from the JSON file
+data = read_prompt_patterns()
+
+# Write the prompt patterns to the specified format and filename
+write_prompt_patterns(data, args.format, args.filename)
