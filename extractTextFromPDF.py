b2fa0786a8a521515a0af66f10ddc8bbd5020914
Tim Haintz <tihaintz@microsoft.com>
2023-08-23 18:32:25 +1000

    Adding new functions for Prompt and content concatenation.

--- extractTextFromPDF.py
+++ extractTextFromPDF.py
@@ -12,8 +12,55 @@ https://learn.microsoft.com/en-us/azure/cognitive-services/openai/how-to/chatgpt
 https://learn.microsoft.com/en-us/azure/cognitive-services/cognitive-services-virtual-networks?tabs=portal
 https://resources.github.com/copilot-for-business/
 '''
+#Note: The openai-python library support for Azure OpenAI is in preview.
+from dotenv import load_dotenv
 import sys
+import os
+import openai
+import json
+from datetime import datetime
+import re
 import PyPDF2
+
+# Load environment variables from the .env file
+load_dotenv()
+
+#############
+# VARIABLES #
+#############
+openai.api_type = "azure"
+openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
+openai.api_version = "2023-05-15"
+openai.api_key = os.getenv("AZURE_OPENAI_KEY")
+
+################################################
+# Enter Prompts Here Separated by '#' character#
+################################################
+prompts = '''# Prompt Example 0.
+# Prompt Example 1.
+# Prompt Example 2.'''
+
+###################################################################################################
+# Enter Instructions Here Separated by '#' character - this can be used in the future for prompts #
+###################################################################################################
+'''# Example prompt instructions below
+# Scope
+-Helpful assistant
+-item Cybersecurity etc
+# Expectations
+- Provide output in bullet points etc.
+- Provide references
+# Style
+- Written in a professional manner
+- Etc
+# Structure
+- Output as JSON
+- Or example output given
+# Grounding
+- Source data to use for grounding
+#Safety
+- You will not provide harmful content
+- Etc'''
 
 def extract_text_from_pdf(pdf_file_name):
     # Open the PDF file in read-binary mode
@@ -36,8 +83,37 @@ def extract_text_from_pdf(pdf_file_name):
             print(f'Text for page {page_num + 1}:')
             print(page_text)
 
+def generate_json(prompts, data):
+    initial_prompt = [
+    {"role": "system", "content": "#Prompt:"},
+    {"role": "user", "content": "#Content:"}
+    ]
+    # Convert the initial prompt to a JSON string
+    prompt_json = json.dumps(initial_prompt)
+
+    # Add the deidentified data and prompt to the prompt variable
+    promptAndContent = prompt_json.replace('"#Prompt:"', f'"{prompts}"')
+    promptAndContent = promptAndContent.replace('"#Content:"', f'"#Content:{data}"')
+
+    # Replace newline characters with the escape sequence \n
+    promptAndContent = promptAndContent.replace('\n', '\\n')
+
+    return promptAndContent
+
 if __name__ == '__main__':
     # Get the file path from the command line arguments
     file_path = sys.argv[1]
     # Replace 'path/to/your/pdf_file.pdf' with the actual file path
     extract_text_from_pdf(file_path)
+
+####################
+# OpenAI CODE #
+####################
+response = openai.ChatCompletion.create(
+    engine="gpt-35-00", # engine = "deployment_name".
+    messages=prompt
+)
+
+# Print the response from the OpenAI API
+print(response['choices'][0]['message']['content'])
+#print(response)
