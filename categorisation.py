c14ae333a98f92d45e5d297b420683b1fd1524f9
Tim Haintz <tihaintz@microsoft.com>
2023-09-29 09:53:31 +1000

    Initial commit

---
+++ categorisation.py
@@ -0,0 +1,21 @@
+'''
+DESCRIPTION
+Using the promptpatterns.json file, categorise the patterns into categories, subcategories and patterns.
+NOTES
+Version:        0.1
+Author:         Tim Haintz                         
+Creation Date:  29/9/2023
+LINKS:
+https://resources.github.com/copilot-for-business/
+https://graphviz.org/
+HELP:
+1. To generate a dot file for each title, run the following command:
+```
+python categorisation.py promptpatterns.json 
+```
+This will generate a dot file for each title in the `graphvizFiles` folder.
+2. 
+```
+python categorisation.py promptpatterns.json ...........
+```
+'''
