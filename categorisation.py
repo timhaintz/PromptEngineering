6c2bc7d4528db30eeba3c29618bce4cac7e2fbe6
Tim Haintz <tihaintz@microsoft.com>
2023-09-29 13:02:58 +1000

    Initial commit and updated formatting.

--- categorisation.py
+++ categorisation.py
@@ -7,7 +7,6 @@ Author:         Tim Haintz
 Creation Date:  29/9/2023
 LINKS:
 https://resources.github.com/copilot-for-business/
-https://graphviz.org/
 HELP:
 1. To generate a dot file for each title, run the following command:
 ```
@@ -19,3 +18,27 @@ This will generate a dot file for each title in the `graphvizFiles` folder.
 python categorisation.py promptpatterns.json ...........
 ```
 '''
+import os
+import datetime
+import json
+
+
+def print_patterns_by_category(category):
+    # Load the JSON data from the file
+    with open('promptpatterns.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    # Get the patterns for the specified category
+    patterns = []
+    for title in data['Source']['Titles']:
+        for cat in title['CategoriesAndPatterns']:
+            if cat['PatternCategory'] == category:
+                patterns += [pattern['PatternName'] for pattern in cat['PromptPatterns']]
+
+    # Print the patterns for the specified category
+    if patterns:
+        print(f"Patterns for {category}:")
+        for pattern in patterns:
+            print(f"- {pattern}")
+    else:
+        print(f"No patterns found for {category}")
---
+++ categorisation_logic.py
@@ -0,0 +1,38 @@
+'''
+DESCRIPTION
+Dictionaries for categories, applications, etc. to map the promptpatterns.json file.
+NOTES
+Version:        0.1
+Author:         Tim Haintz                         
+Creation Date:  29/9/2023
+LINKS:
+https://resources.github.com/copilot-for-business/
+HELP:
+1. To use the categories dictionary, import the library using the following command:
+```
+from categorisation_logic import categories
+```
+This will use the categories dictionary.
+2. To use the application dictionary, import the library using the following command:
+```
+from categorisation_logic import application
+```
+'''
+
+categories = {
+    'Input Semantics': ['Pattern 1', 'Pattern 2', 'Pattern 3'],
+    'Output Customization': ['Pattern 4', 'Pattern 5', 'Pattern 6'],
+    'Error Identification': ['Pattern 7', 'Pattern 8', 'Pattern 9'],
+    'Prompt Improvement': ['Pattern 10', 'Pattern 11', 'Pattern 12'],
+    'Refactoring': ['Pattern 13', 'Pattern 14', 'Pattern 15'],
+    'Interaction': ['Pattern 16', 'Pattern 17', 'Pattern 18'],
+    'Context Control': ['Pattern 19', 'Pattern 20', 'Pattern 21'],
+    'Requirements Elicitation': ['Pattern 22', 'Pattern 23', 'Pattern 24'],
+    'System Design and Simulation': ['Pattern 25', 'Pattern 26', 'Pattern 27'],
+    'Mathematics': ['Pattern 28', 'Pattern 29', 'Pattern 30'],
+    'Jailbreaking': ['Pattern 31', 'Pattern 32', 'Pattern 33'],
+    'Self-consistency': ['Pattern 34', 'Pattern 35', 'Pattern 36'],
+    'Logic and Reasoning': ['Pattern 37', 'Pattern 38', 'Pattern 39'],
+    'Decomposed Prompting': ['Pattern 40', 'Pattern 41', 'Pattern 42']
+
+}
