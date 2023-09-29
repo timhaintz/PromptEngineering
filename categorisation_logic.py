b3204f1a8a4d396848ac2879d135a1c25979020e
Tim Haintz <tihaintz@microsoft.com>
2023-09-29 13:39:56 +1000

    Added more categories and patterns and also updated the dictionary name.

--- categorisation_logic.py
+++ categorisation_logic.py
@@ -10,7 +10,7 @@ https://resources.github.com/copilot-for-business/
 HELP:
 1. To use the categories dictionary, import the library using the following command:
 ```
-from categorisation_logic import categories
+from categorisation_logic import categoriesAndPatterns
 ```
 This will use the categories dictionary.
 2. To use the application dictionary, import the library using the following command:
@@ -19,16 +19,16 @@ from categorisation_logic import application
 ```
 '''
 
-categories = {
-    'Input Semantics': ['Pattern 1', 'Pattern 2', 'Pattern 3'],
-    'Output Customization': ['Pattern 4', 'Pattern 5', 'Pattern 6'],
-    'Error Identification': ['Pattern 7', 'Pattern 8', 'Pattern 9'],
-    'Prompt Improvement': ['Pattern 10', 'Pattern 11', 'Pattern 12'],
-    'Refactoring': ['Pattern 13', 'Pattern 14', 'Pattern 15'],
-    'Interaction': ['Pattern 16', 'Pattern 17', 'Pattern 18'],
-    'Context Control': ['Pattern 19', 'Pattern 20', 'Pattern 21'],
-    'Requirements Elicitation': ['Pattern 22', 'Pattern 23', 'Pattern 24'],
-    'System Design and Simulation': ['Pattern 25', 'Pattern 26', 'Pattern 27'],
+categoriesAndPatterns = {
+    'Input Semantics': ['Meta Language Creation', 'Pattern 2', 'Pattern 3'],
+    'Output Customization': ['Output Automater', 'Persona', 'Visualization Generator', 'Recipe', 'Template'],
+    'Error Identification': ['Fact Check List', 'Reflection'],
+    'Prompt Improvement': ['Question Refinement', 'Alternative Approaches', 'Cognitive Verifier', 'Refusal Breaker'],
+    'Refactoring': ['Pseudo-code Refactoring', 'Data-guided Refactoring'],
+    'Interaction': ['Flipped Interaction', 'Game Play', 'Infinite Generation'],
+    'Context Control': ['Context Manager'],
+    'Requirements Elicitation': ['Requirements Simulator', 'Specification Disambiguation', 'Change Request Simulation'],
+    'System Design and Simulation': ['API Generator', 'API Simulator', 'Few-shot Example Generator', 'Domain-Specific Language (DSL) Creation', 'Architectural Possibilities'],
     'Mathematics': ['Pattern 28', 'Pattern 29', 'Pattern 30'],
     'Jailbreaking': ['Pattern 31', 'Pattern 32', 'Pattern 33'],
     'Self-consistency': ['Pattern 34', 'Pattern 35', 'Pattern 36'],
