e0b1be5917005f1a062267de8ae9f88871099cfa
Tim Haintz <tihaintz@microsoft.com>
2023-10-02 09:23:17 +1100

    Added application to cattegoriesAndPatterns mapping.

--- categorisation_logic.py
+++ categorisation_logic.py
@@ -21,7 +21,7 @@ from categorisation_logic import application
 
 categoriesAndPatterns = {
     'Input Semantics': ['Meta Language Creation', 'Free-Form Questions'],
-    'Output Customization': ['Output Automater', 'Persona', 'Visualization Generator', 'Recipe', 'Template'],
+    'Output Customisation': ['Output Automater', 'Persona', 'Visualization Generator', 'Recipe', 'Template'],
     'Error Identification': ['Fact Check List', 'Reflection', 'Restrict'],
     'Prompt Improvement': ['Question Refinement', 'Alternative Approaches', 'Cognitive Verifier', 'Refusal Breaker'],
     'Refactoring': ['Pseudo-code Refactoring', 'Data-guided Refactoring', 'Code Clustering', 'Intermediate Abstraction', 'Principled Code', 'Hidden Assumptions'],
@@ -41,7 +41,18 @@ pattern_descriptions = {
     'Multi-Criteria Rating': 'This pattern refers to Pattern Category: N/A and Pattern Name Expert #1 - #4 in promptpatterns.json.',
 }
 
-application = {
-
-}
+application = { 
+    'Chatbot': ['Input Semantics', 'Output Customization', 'Error Identification', 'Prompt Improvement', 'Interaction', 'Context Control', 'Logic and Reasoning', 'Language and Semantics', 'Evaluation and Rating'],
+    'Classification': ['Input Semantics', 'Output Customisation', 'Error Identification', 'Prompt Improvement', 'Refactoring', 'Mathematics', 'Logic and Reasoning', 'Decomposed Prompting', 'Language and Semantics'],
+    'Clustering': ['Input Semantics', 'Output Customization', 'Error Identification', 'Prompt Improvement', 'Refactoring', 'Mathematics', 'Logic and Reasoning'],
+    'Content Creation': ['Input Semantics', 'Output Customization', 'Error Identification', 'Prompt Improvement', 'Refactoring', 'Interaction', 'Context Control', 'Requirements Elicitation', 'System Design and Simulation'], 
+    'Customer Service': ['Input Semantics', 'Output Customization', 'Error Identification', 'Prompt Improvement', 'Interaction', 'Context Control'], 
+    'Data Science': ['Input Semantics', 'Output Customization', 'Error Identification', 'Prompt Improvement', 'Refactoring', 'Mathematics'], 
+    'Healthcare': ['Input Semantics', 'Output Customization', 'Error Identification', 'Prompt Improvement', 'Interaction'], 
+    'Programming': ['Input Semantics','Output Customization','Error Identification','Prompt Improvement','Refactoring','System Design and Simulation','Jailbreaking','Decomposed Prompting'], 
+    'Question Answering': ['Input Semantics','Output Customization','Error Identification','Prompt Improvement','Interaction','Logic and Reasoning','Language and Semantics'],
+    'Sentiment Analysis': ['Input Semantics','Output Customization','Error Identification','Prompt Improvement','Language and Semantics'], 
+    'Summarisation': ['Input Semantics','Output Customization','Error Identification','Prompt Improvement','Language and Semantics'], 
+    'Translation': ['Input Semantics','Output Customization','Error Identification','Prompt Improvement','Language and Semantics'] 
+    }
 
